7c0c16f54e6f6d602b25e797ae238dc4d0a795fc
Lucasus <lukasz.wiatrak@gmail.com>
2017-10-14 22:51:29 +0200

    Fresh start with create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.css';
+const logo = require('./logo.svg');
+class App extends Component {
+    render() {
+        return (<div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Welcome to React123555</h2>
+        </div>
+        <p className="App-intro">
+          To getfdfd started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>);
+    }
+}
+export default App;
+//# sourceMappingURL=App.jsx.map
--- src/components/sample.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export class Sample extends React.Component<{ content: string }, {}> {
-  render() {
-    return <div>{this.props.content}</div>
-  }
-}
